c13282b2ad23264c69860c854ce9c98a7dd2f75b
Howakyl96 <howakyl@gmail.com>
2020-10-26 14:15:23 -0700

    commit 3 - birds about url

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 #HOME VIEW
 def home(request):
     return HttpResponse('<h1>hello!</h1>')
+
+#ABOUT VIEW
+def about(request):
+    return HttpResponse('<h1>Let me tell ya about BIRDS.</h1>')
