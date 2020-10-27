7271c13b6295d939a1585f235d9f84295a660da5
Howakyl96 <howakyl@gmail.com>
2020-10-27 13:37:08 -0700

    commit 7 - made changes to app name data

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,12 @@ class Bird:
         self.likes = likes
         self.native_to = native_to
 
+# class Bird(models.Model): 
+#         species = models.CharField(max_length=200)
+#         likes = models.CharField(max_length=200)
+#         native_to = models.CharField(max_length=100)
+
 birds = [
     Bird('Great Horned Owl' , 'rodents' , 'North/South America' ),
-    Bird('Steller\'s Jay' , 'nuts, berries' , 'Western North America')
+    Bird('Steller\'s Jay' , 'nuts, berries' , 'Western North America'),
 ]
