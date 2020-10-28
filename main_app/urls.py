7aa668927534d3ee813a97933895b42f4b1516d5
Howakyl96 <howakyl@gmail.com>
2020-10-28 16:25:08 -0700

    commit 17 - habitats can now be added to birds on details view

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/' , views.about , name='about'),
     path('birds/' , views.birds_index , name='index'),
     path('birds/<int:bird_id>/' , views.birds_detail , name='detail'),
-    path('birds/<int:bird_id>/add_spotting/' , views.add_spotting, name='add_spotting')
+    path('birds/<int:bird_id>/add_spotting/' , views.add_spotting, name='add_spotting'),
+    path('birds/<int:bird_id>/assoc_habitat/<int:habitat_id>/', views.assoc_habitat, name='assoc_habitat')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from .models import Bird
+from .models import Bird, Habitat
 from .forms import SpottingForm
 
 #HOME VIEW
 def home(request):
     return render(request, 'home.html')
 
+
 #ABOUT VIEW
 def about(request):
     return render(request, 'about.html')
+
 
 # BIRD INDEX
 def birds_index(request):
     birds = Bird.objects.all()
     return render(request, 'birds/index.html', {'birds': birds})
 
+
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
+    habitats_bird_doesnt_have = Habitat.objects.exclude(id__in = bird.habitats.all().values_list('id'))
     spotting_form = SpottingForm()
 
     return render(request, 'birds/detail.html', {
         'bird': bird,
-        'spotting_form': spotting_form
+        'spotting_form': spotting_form,
+        'habitats': habitats_bird_doesnt_have
         })
+
+
+def assoc_habitat(request, bird_id, habitat_id):
+    Bird.objects.get(id=bird_id).habitats.add(habitat_id)
+    return redirect('detail', bird_id=bird_id)
+
 
 def add_spotting(request, bird_id):
     form = SpottingForm(request.POST)
