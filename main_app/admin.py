8a66713a57a0fef7d0aeac591569ff5d7514967b
Howakyl96 <howakyl@gmail.com>
2020-10-28 14:59:46 -0700

    commit 16 - habitats showing on birds detail pages

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bird, Spotting
+from .models import Bird, Spotting, Habitat
 
 admin.site.register(Bird)
 admin.site.register(Spotting)
+admin.site.register(Habitat)
